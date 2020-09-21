48d2c6fe92a0ca405835a2b0c1d3ace1d5f0f8a0
Jaimin Belani <jaimin@coodeit.com>
2020-09-21 12:19:50 +0530

    Updates

---
+++ precache-manifest.8a157e2582856d75a2e0a97d4cd59352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0520e8690245db3521201722b4ff791",
+    "url": "/react-dynamic-draggable-components/index.html"
+  },
+  {
+    "revision": "430921be52f3bb8caf86",
+    "url": "/react-dynamic-draggable-components/static/js/2.0d46d5e4.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/react-dynamic-draggable-components/static/js/2.0d46d5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0919c828a5a52b73c091",
+    "url": "/react-dynamic-draggable-components/static/js/main.8e960255.chunk.js"
+  },
+  {
+    "revision": "d16160569ee89c5b8205",
+    "url": "/react-dynamic-draggable-components/static/js/runtime-main.1e7cd34e.js"
+  }
+]);
